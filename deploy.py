1a3c0404888ceda84d0d711d22065a673546aaf9
CronyAkatsuki <cronyakatsuki@gmail.com>
2021-06-20 14:28:33 +0200

    new content added

--- deploy.py
+++ deploy.py
@@ -1,39 +1,39 @@
 import os
 import git
-from datetime import datetime
 
 # print message
 print("\033[0;32mBuilding website with hugo...\033[0m\n")
 
 # run hugo
-# os.system("hugo")
+os.system("hugo")
 
 # changing directory
 os.chdir("public")
-print (os.getcwd())
 
+# getting current repo
 repo = git.Repo(os.getcwd())
 
-print(datetime.date(datetime.now()))
-
-
+# if any changes for the website push it
 if repo.is_dirty(untracked_files=True):
-    print('Changes detected.')
+    print("\033[0;32mRebuilding website...\033[0m\n")
     repo.git.add('--all')
-    repo.git.commit('-m','Rebuilding site ', print(datetime.date(datetime.now())))
+    repo.git.commit('-m','rebuilding site')
     print(repo.remotes.origin.push('master'))
 else:
-    print('no changes', print(datetime.date(datetime.now())))
+    print('no changes')
 
+# going back
 os.chdir("..")
-print (os.getcwd())
 
 repo = git.Repo(os.getcwd())
 
+# checking for new content and pushing it
 if repo.is_dirty(untracked_files=True):
-    print('Changes detected.')
+    print("\033[0;32mPushing new content...\033[0m\n")
     repo.git.add('--all')
-    repo.git.commit('-m', "rebuilding site ", print(datetime.date(datetime.now())))
+    repo.git.commit('-m', "new content added")
     print(repo.remotes.origin.push('master'))
 else:
-    print('no changes', print(datetime.date(datetime.now())))
+    print('no changes')
+
+print("\033[0;32mDeployment finished...\033[0m\n")
