9b38caa7b825fdb95f29cd70117cae1d03debb17
CronyTips <67902236+CronyTips@users.noreply.github.com>
2021-07-14 09:42:35 +0200

    New content added

--- deploy.py
+++ deploy.py
@@ -1,5 +1,4 @@
-import os
-import git
+import os, git, subprocess
 
 # print message
 print("\033[0;32mBuilding website with hugo...\033[0m\n")
@@ -16,9 +15,9 @@ repo = git.Repo(os.getcwd())
 # if any changes for the website push it
 if repo.is_dirty(untracked_files=True):
     print("\033[0;32mRebuilding website...\033[0m\n")
-    repo.git.add('--all')
-    repo.git.commit('-m','rebuilding site')
-    print(repo.remotes.origin.push('master'))
+    subprocess.call("git add --all")
+    subprocess.call('git commit -m "Rebuilding website" ')
+    subprocess.call('git push origin master')
 else:
     print('no changes')
 
@@ -30,9 +29,9 @@ repo = git.Repo(os.getcwd())
 # checking for new content and pushing it
 if repo.is_dirty(untracked_files=True):
     print("\033[0;32mPushing new content...\033[0m\n")
-    repo.git.add('--all')
-    repo.git.commit('-m', "new content added")
-    print(repo.remotes.origin.push('master'))
+    subprocess.call("git add --all")
+    subprocess.call('git commit -m "New content added" ')
+    subprocess.call('git push origin master')
 else:
     print('no changes')
 
